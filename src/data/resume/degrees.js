d2a49cc8779046f8c80ef5e5a19844ed91083253
Luke Goodson <lukejgoodson@gmail.com>
2024-10-05 21:20:52 +0100

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -6,6 +6,12 @@ const degrees = [
     year: 'ongoing',
   },
   {
+    school: 'Open University',
+    degree: 'CCNA - Cisco',
+    link: 'https://www.open.ac.uk/',
+    year: 2024,
+  },
+  {
     school: 'Cardiff University',
     degree: 'Computer Science with Security and Forensics',
     link: 'https://www.cardiff.ac.uk/',
