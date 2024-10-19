9534074766b82ef3739c666d69ad626e2b12a297
Luke Goodson <lukejgoodson@gmail.com>
2024-10-19 14:18:07 +0100

    Updating Single/Double quotes

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe('Luke Goodson');
+    expect(document.title).toBe("Luke Goodson");
   });
 
   it('can navigate to /about', async () => {
